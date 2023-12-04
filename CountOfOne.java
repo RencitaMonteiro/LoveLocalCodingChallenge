9cf4266ca8ae1f25a33be036e0c87f3e7f882842
Rencita Monteiro <106856518+RencitaMonteiro@users.noreply.github.com>
2023-12-04 23:29:12 +0530

    Update CountOfOne.java

--- CountOfOne.java
+++ CountOfOne.java
@@ -8,22 +8,22 @@ import java.util.Scanner;
 public class CountOfOne {
     public static void main(String args[])
     {
-        Scanner sc = new Scanner(System.in);
-        int i,temp=0,c=0,j=0;
+        Scanner sc = new Scanner(System.in); //Create oject to input data 
+        int i,temp=0,c=0,j=0; //Initialization 
         System.out.println("Enter the limit");
-        int n = sc.nextInt();
-        for(i=0;i<=n;i++)
+        int n = sc.nextInt(); // Input the data 
+        for(i=0;i<=n;i++) //traverve from 0 to the limit
         {
-          j = i;
+          j = i;  
           while(j>0)
           {
-            temp = j%10;
-            if(temp==1)
-               c++;
+            temp = j%10; // retrieve the last digit 
+            if(temp==1) // check if its equal to 1 
+               c++; // if its 1 , increement counter 
                
-            j = j/10;
+            j = j/10; //discard the lst digit , in order to check for preceeding numbers 
           }
         }
-        System.out.println(c);
+        System.out.println(c); //  Print the number of 1's 
     }
 }
